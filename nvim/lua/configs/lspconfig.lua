b111898078f48af5c2b45dab8cbd8d19afbe06fa
Ice-wolf11 <andrewmedina2227@gmail.com>
2024-08-13 21:48:30 -0500

    agregando la configuracion de nvim

---
+++ nvim/lua/configs/lspconfig.lua
@@ -0,0 +1,24 @@
+-- load defaults i.e lua_lsp
+require("nvchad.configs.lspconfig").defaults()
+
+local lspconfig = require "lspconfig"
+
+-- EXAMPLE
+local servers = { "html", "cssls","pretty-php","angular-language-server","blade-formatter","eslint-lsp","isort","prettier","pyright","sqlls","sql-formatter","typescript-language-server","intelephense" }
+local nvlsp = require "nvchad.configs.lspconfig"
+
+-- lsps with default config
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
+end
+
+-- configuring single server, example: typescript
+-- lspconfig.tsserver.setup {
+--   on_attach = nvlsp.on_attach,
+--   on_init = nvlsp.on_init,
+--   capabilities = nvlsp.capabilities,
+-- }
